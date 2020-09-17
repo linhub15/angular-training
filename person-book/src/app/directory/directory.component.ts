34914ea1cff8eb4b9d8fb99b1a4fda749f975f9f
linhub <linhub15@gmail.com>
2020-09-17 11:28:05 -0600

    fix loop

--- person-book/src/app/directory/directory.component.ts
+++ person-book/src/app/directory/directory.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { PeopleService } from '../core/http/people/people.service';
+import { Person } from '../core/http/people/person.model';
 
 @Component({
   selector: 'app-directory',
@@ -7,12 +9,11 @@ import { PeopleService } from '../core/http/people/people.service';
   styleUrls: ['./directory.component.scss']
 })
 export class DirectoryComponent implements OnInit {
+  people$: Observable<Person[]>;
 
   constructor(private peopleService: PeopleService) { }
 
-  ngOnInit(): void {}
-
-  getPeople$() {
-    return this.peopleService.getPeople$();
+  ngOnInit(): void {
+    this.peopleService.getPeople$();
   }
 }
