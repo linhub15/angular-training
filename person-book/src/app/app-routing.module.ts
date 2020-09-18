8e8262f4f52f87210cd09143417575e060902cec
linhub <linhub15@gmail.com>
2020-09-18 11:34:11 -0600

    add auth guard to register route

--- person-book/src/app/app-routing.module.ts
+++ person-book/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { AuthGuard } from './core/auth.guard';
 import { AddPersonComponent } from './directory/add-person/add-person.component';
 import { DirectoryComponent } from './directory/directory.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'directory', pathMatch: 'full'},
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'register', component: RegisterComponent, canActivate: [AuthGuard] },
   { path: 'directory', component: DirectoryComponent },
   { path: 'add-person', component: AddPersonComponent }
 ];
