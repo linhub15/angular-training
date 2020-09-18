191f9a12bc3032f22f6a2e726145b21a24c667c2
linhub <linhub15@gmail.com>
2020-09-18 11:11:45 -0600

    fix name collision

--- person-book/src/app/core/http-jwt.interceptor.ts
+++ person-book/src/app/core/http-jwt.interceptor.ts
@@ -8,7 +8,7 @@ import {
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class HttpInterceptor implements HttpInterceptor {
+export class HttpJwtInterceptor implements HttpInterceptor {
 
   constructor() {}
 
